5747b38bde6154cd5344b9491716ecc524569376
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-06-20 15:35:54 +0700

    update mason dap config

--- init.lua
+++ init.lua
@@ -2,3 +2,13 @@
 require("config.lazy")
 
 vim.cmd([[colorscheme dracula]])
+
+require("mason-nvim-dap").setup({
+  ensure_installed = { "coreclr" },
+  automatic_installation = true,
+})
+
+require("nvim-dap-virtual-text").setup({
+  all_frames = true,
+  highlight_new_as_changed = true,
+})
--- lua/plugins/masonDap.lua
+++ lua/plugins/masonDap.lua
@@ -1,29 +1,33 @@
-require("mason-nvim-dap").setup({
-  ensure_installed = { "coreclr" },
-  automatic_installation = true,
-})
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  dependencies = "mason.nvim",
+  cmd = { "DapInstall", "DapUninstall" },
+  init = function()
+    local dap = require("dap")
+    local daputil = require("dap.utils")
 
-local dap = require("dap")
-local daputil = require("dap.utils")
-
-dap.configurations.cs = {
-  {
-    type = "coreclr",
-    name = "launch - DMC Api",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to dll", vim.fn.getcwd() .. "\\bin\\Debug\\", "file")
-    end,
-  },
-  {
-    type = "coreclr",
-    name = "attach - coreclr",
-    request = "attach",
-    processId = daputil.pick_process,
-    program = function()
-      return vim.fn.input("Path to dll", vim.fn.getcwd() .. "\\bin\\Debug\\", "file")
-    end,
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - DMC Api",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to dll", vim.fn.getcwd() .. "\\bin\\Debug\\", "file")
+        end,
+      },
+      {
+        type = "coreclr",
+        name = "attach - coreclr",
+        request = "attach",
+        processId = daputil.pick_process,
+        program = function()
+          return vim.fn.input("Path to dll", vim.fn.getcwd() .. "\\bin\\Debug\\", "file")
+        end,
+      },
+    }
+  end,
+  opts = {
+    ensure_installed = { "coreclr" },
+    automatic_installation = true,
   },
 }
-
-return {}
