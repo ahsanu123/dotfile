78b94a3fb2545aba429668199f953ad2505f6b5e
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-12-17 10:28:12 +0700

    [12-17-1028] adding lvim config from wsl

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd([[colorscheme dracula]])
+vim.cmd("set cursorcolumn")
+vim.cmd("hi CursorColumn term=bold cterm=bold guibg=Grey30")
