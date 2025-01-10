7c8c6100504aac4ba193b817a5b194d7142d970b
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-01-10 17:35:48 +0700

    [01-10-1735] add razor page lsp config with rzls nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+require("mason").setup({
+  registries = {
+    "github:mason-org/mason-registry",
+    "github:crashdummyy/mason-registry",
+  },
+})
+
 require("neogen").setup({
   languages = {
     cs = {
